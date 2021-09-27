d113f4f8ede974d624f0cdb9fb9c23d028b32b3d
jemblonganvalley <jemblonganvalley@gmail.com>
2021-09-27 23:42:28 +0700

    update

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -15,7 +15,7 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/users_read", middlewares.SetMiddlewareJSON(s.GetUsers)).Methods("GET")
 	s.Router.HandleFunc("/users_update/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateUser))).Methods("PUT")
 	s.Router.HandleFunc("/users_delete/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteUser)).Methods("DELETE")
-	s.Router.HandleFunc("/users_find/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("GET")
+	s.Router.HandleFunc("/users_find/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("POST")
 
 	//Posts routes
 	s.Router.HandleFunc("/posts_create", middlewares.SetMiddlewareJSON(s.CreatePost)).Methods("POST")
